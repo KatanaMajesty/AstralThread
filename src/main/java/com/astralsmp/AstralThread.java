58e75749f0b2841e8240047a4d5cce647a8b2bc1
KatanaMajesty <sasha.panasenk@gmail.com>
2021-07-12 23:32:34 +0300

    Настройка

---
+++ src/main/java/com/astralsmp/AstralThread.java
@@ -0,0 +1,17 @@
+package com.astralsmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AstralThread extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
